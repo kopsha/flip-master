90dadc1de93cb7b80b4fac5cf3a2be0b95e24f25
Florin Ciurcanu <florin.ciurcanu@gmail.com>
2022-05-14 08:08:02 +0300

    moved to mackie

---
+++ metaflip.py
@@ -0,0 +1,57 @@
+#!/usr/bin/env python3
+from collections import namedtuple
+from dataclasses import dataclass
+from datetime import datetime
+from enum import Enum
+
+
+# Binance API response kline format
+KLinePoint = namedtuple(
+    "KLinePoint",
+    [
+        "open_time",
+        "open",
+        "high",
+        "low",
+        "close",
+        "volume",
+        "close_time",
+        "quote_asset_volume",
+        "trades_count",
+        "taker_buy_base_asset_volume",
+        "taker_buy_quote_asset_volume",
+        "ignore",
+    ],
+)
+
+
+@dataclass
+class PricePoint:
+    """Main flippie data structure"""
+    open_time: datetime
+    open: float
+    close: float
+    close_time: datetime
+
+    def __init__(self, kline_data):
+        kpoint = KLinePoint(*kline_data)
+        self.open_time = datetime.fromtimestamp(kpoint.open_time // 1000)
+        self.open = float(kpoint.open)
+        self.close = float(kpoint.low)
+        self.close_time = datetime.fromtimestamp(kpoint.close_time // 1000)
+
+
+class FlipSignals(Enum):
+    HOLD = 0
+    ENTRY = 1
+    SELL = 2
+    BUY = 3
+    EXIT = 4
+
+    AS_STR = {
+        0: "hold",
+        1: "entry",
+        2: "sell",
+        3: "buy",
+        4: "exit",
+    }
