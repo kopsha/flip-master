8293cedf59a49c88001a3c8fa50517feaa8386e8
Florin Ciurcanu <florin.ciurcanu@gmail.com>
2022-06-04 17:14:28 +0300

    still fighting with params

--- pinkybrain.py
+++ pinkybrain.py
@@ -1,9 +1,8 @@
 from decimal import Decimal
-from math import isclose
 import pandas as pd
 import mplfinance as mpf
 from ta.volatility import BollingerBands
-from ta.momentum import tsi, awesome_oscillator
+from ta.momentum import tsi
 from ta.volume import money_flow_index
 from metaflip import CandleStick, FlipSignals
 
@@ -21,10 +20,10 @@ class PinkyTracker:
         89,
         144,
     ]
-    MFI_HIGH = 70
-    MFI_LOW = 30
+    MFI_HIGH = 65
+    MFI_LOW = 27
     TSI_TOLERANCE = 10
-    VELOCITY_TOLERANCE = 5
+    VELOCITY_TOLERANCE = 8
 
     def __init__(self, trading_pair, budget, commission, wix):
         self.base_symbol, self.quote_symbol = trading_pair
@@ -34,12 +33,14 @@ class PinkyTracker:
         self.is_committed = False
 
         # some parameters
-        self.stop_loss_factor = self.commission * 5
+        self.stop_loss_factor = commission * 4
         self.wix = wix
 
         # running data
         self.data = pd.DataFrame()
         self.order_history = list()
+        self.commit_price = None
+        self.stop_loss = None
 
     def __repr__(self) -> str:
         last_price = self.data["close"].iloc[-1]
@@ -74,6 +75,8 @@ class PinkyTracker:
         self.data = pd.concat([self.data.tail(1000 - new_df.size), new_df])
 
         # add indicators to dataframe
+        self.data["velocity"] = self.data["close"].diff()
+
         bb = BollingerBands(close=self.data["close"], window=self.window)
         self.data["bb_ma"] = bb.bollinger_mavg()
         self.data["bb_high"] = bb.bollinger_hband()
@@ -101,25 +104,24 @@ class PinkyTracker:
             volume=df["volume"],
             window=self.window,
         )
-        self.data["aosc"] = awesome_oscillator(
-            high=df["high"],
-            low=df["low"],
-        )
 
     def buy_in(self, itime, price):
         self.is_committed = True
 
-        amount = self.quote
+        spent = self.quote
         bought = (1 - self.commission) * self.quote / price
 
-        self.quote -= amount
+        self.quote -= spent
         self.base += bought
         self.order_history.append((FlipSignals.BUY, itime, price))
 
+        self.spent = spent
+        self.commit_price = Decimal(price)
+        self.stop_loss = price * (1 - self.stop_loss_factor)
+
         print(
-            f"Bought {bought} {self.base_symbol} at {price} {self.quote_symbol} [{amount} {self.quote_symbol}]"
+            f"Bought {bought} {self.base_symbol} at {price} {self.quote_symbol}, spent {spent} {self.quote_symbol}]"
         )
-        print(self)
 
     def sell_out(self, itime, price):
         self.is_committed = False
@@ -129,19 +131,23 @@ class PinkyTracker:
 
         self.base -= sold
         self.quote += amount
-
         self.order_history.append((FlipSignals.SELL, itime, price))
 
+        self.commit_price = None
+        self.stop_loss = None
+
+        profit = amount - self.spent
+
         print(
-            f" Sold  {sold} {self.base_symbol} at {price} {self.quote_symbol} [{amount} {self.quote_symbol}]"
+            f" Sold  {sold} {self.base_symbol} at {price} {self.quote_symbol} for {profit} {self.quote_symbol} profit [Wallet: {self.quote} {self.quote_symbol}]"
         )
         print(self)
 
     def backtest(self):
         next_move = None
+        tolerance = 0.0015
         for i, (_, row) in enumerate(self.data.iterrows()):
             price = float(row["low"])
-            tolerance = 0.001
             if not self.is_committed:
                 if (
                     next_move == FlipSignals.BUY
@@ -153,23 +159,31 @@ class PinkyTracker:
                     next_move is None
                     and row["mfi"] < self.MFI_LOW
                     and row["tsi"] > -self.TSI_TOLERANCE
-                    and (price * (1 - tolerance)) <= row["bb_low"]
+                    and price <= (row["bb_low"] * (1 + tolerance))
                 ):
                     if (row["trend_velocity"] + self.VELOCITY_TOLERANCE) > 0:
                         self.buy_in(i, row["close"])
                     else:
                         next_move = FlipSignals.BUY
             else:
+                if float(row["close"]) <= self.stop_loss:
+                    print("WARNING: Stop loss activated.")
+                    self.sell_out(i, row["close"])
+                    next_move = None
+                    continue
+
                 if (
                     next_move == FlipSignals.SELL
-                    and (row["trend_velocity"] - self.VELOCITY_TOLERANCE) < 0
+                    # and (row["trend_velocity"] - self.VELOCITY_TOLERANCE) < 0
+                    and row["velocity"] <= 0
                 ):
                     self.sell_out(i, row["close"])
                     next_move = None
                 elif (
                     next_move is None
                     and row["mfi"] > self.MFI_HIGH
-                    and (price * (1 + tolerance)) >= row["bb_high"]
+                    and price >= (row["bb_high"] * (1 - tolerance))
+                    and row["velocity"] <= 0
                 ):
                     if (row["trend_velocity"] - self.VELOCITY_TOLERANCE) > 0:
                         self.sell_out(i, row["close"])
@@ -193,8 +207,6 @@ class PinkyTracker:
         df["mfi-high"] = self.MFI_HIGH
         df["mfi-low"] = self.MFI_LOW
 
-        aosc_colors = ["green" if x >= 0 else "red" for x in df["aosc"].diff()]
-
         extras = [
             mpf.make_addplot(df["bb_high"], color="olive", secondary_y=False, alpha=0.35),
             mpf.make_addplot(df["bb_ma"], color="blue", secondary_y=False, alpha=0.35),
@@ -217,8 +229,6 @@ class PinkyTracker:
             mpf.make_addplot(
                 df["mfi-low"], color="brown", alpha=0.35, secondary_y=False, panel=2
             ),
-
-            mpf.make_addplot(df["aosc"], type="bar", color=aosc_colors, panel=3),
         ]
 
         fig, axes = mpf.plot(
