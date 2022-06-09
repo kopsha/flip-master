461c741b8af85a4e55e1187644361b28eeee8760
Florin Ciurcanu <florin.ciurcanu@gmail.com>
2022-06-09 07:53:28 +0300

    telegram bot was a quickie

---
+++ src/notifier.py
@@ -0,0 +1,16 @@
+import requests
+
+
+class TelegramNotifier:
+    def __init__(self, token, owner_id):
+        self.token = token
+        self.owner_id = owner_id
+
+    def say(self, message):
+        url = (
+            "https://api.telegram.org/bot{token}/sendMessage?"
+            "chat_id={chat_id}&parse_mode=Markdown&text={message}"
+        ).format(token=self.token, chat_id=self.owner_id, message=message)
+
+        response = requests.get(url)
+        return response.json()
